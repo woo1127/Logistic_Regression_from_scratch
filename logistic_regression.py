7d8c466f45b4a58082177d026b50cfac2c5b7efd
Jeremy Woo <71725118+woo1127@users.noreply.github.com>
2021-12-02 18:26:47 +0800

    adding new function

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,32 +1,37 @@
-import numpy as np
+import numpy
 import warnings
 
-class LogisticRegression():
+class Logistics:
     warnings.filterwarnings('ignore')
     
     def __init__(self, alpha=0.001, iteration=20000, split_ratio=0.75):
         self.alpha = alpha
         self.iteration = iteration
-        self.split_ratio = split_ratio
+        self.ratio = split_ratio
     
     
     def train_test_split(self, X, y):
-        self.X = np.insert(np.array(X), 0, 0, axis=1)
-        self.y = np.array(y)
-        mask = np.random.rand(len(self.X)) < self.split_ratio
+        mask = np.random.rand(len(self.X)) < self.ratio
         
         return self.X[mask], self.y[mask], self.X[~mask], self.y[~mask]
         
         
     def fit(self, X, y):
-        self.X_train, self.y_train, self.X_test, self.y_test = self.train_test_split(X, y)
+        self.X = np.insert(np.array(X), 0, 0, axis=1)
+        self.y = np.array(y)
+        self.X_train, self.y_train, self.X_test, self.y_test = self.train_test_split(self.X, self.y)
         
         row, column = self.X_train.shape
         self.theta = np.zeros(column)
         
+        
         for _ in range(self.iteration):
-            y_hat = 1 / (1 + np.exp( - (self.X_train @ self.theta) ))
-            self.theta = self.theta - self.alpha * ( ( (y_hat - self.y_train) @ self.X_train ) / row)
+            self.training_model(row)
+            
+    
+    def training_model(self, row):
+        y_hat = 1 / (1 + np.exp( - (self.X_train @ self.theta) ))
+        self.theta = self.theta - self.alpha * ( ( (y_hat - self.y_train) @ self.X_train ) / row)
             
             
     def predict(self):
@@ -40,3 +45,5 @@ class LogisticRegression():
         compare = self.y_pred == self.y_test
         
         return len([i for i in compare if i == True]) / len(self.X_test) * 100
+    
+    
