46422ed2d7a246a3c12aa346804a6fd7dae3acfb
Jeremy Woo <71725118+woo1127@users.noreply.github.com>
2022-03-29 16:09:55 +0800

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,8 +1,8 @@
 import numpy as np
 import warnings
+warnings.filterwarnings('ignore')
 
 class LogisticRegression:
-    warnings.filterwarnings('ignore')
     
     def __init__(self, alpha=0.001, iteration=20000, split_ratio=0.75):
         self.alpha = alpha
@@ -10,7 +10,7 @@ class LogisticRegression:
         self.ratio = split_ratio
     
     
-    def train_test_split(self, X, y):
+    def _train_test_split(self, X, y):
         mask = np.random.rand(len(self.X)) < self.ratio
         
         return self.X[mask], self.y[mask], self.X[~mask], self.y[~mask]
@@ -19,17 +19,17 @@ class LogisticRegression:
     def fit(self, X, y):
         self.X = np.insert(np.array(X), 0, 1, axis=1)
         self.y = np.array(y)
-        self.X_train, self.y_train, self.X_test, self.y_test = self.train_test_split(self.X, self.y)
+        self.X_train, self.y_train, self.X_test, self.y_test = self._train_test_split(self.X, self.y)
         
         row, column = self.X_train.shape
         self.theta = np.zeros(column)
         
         
         for _ in range(self.iteration):
-            self.training_model(row)
+            self._training_model(row)
             
     
-    def training_model(self, row):
+    def _training_model(self, row):
         y_hat = 1 / (1 + np.exp( - (self.X_train @ self.theta) ))
         self.theta = self.theta - self.alpha * ( ( (y_hat - self.y_train) @ self.X_train ) / row)
             
